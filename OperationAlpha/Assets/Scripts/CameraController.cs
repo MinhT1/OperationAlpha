5f413483683c27a9d9c3c513029a15981244c8e8
3cD0 <mrdavid3@yahoo.com>
2021-10-19 20:08:16 -0400

    FPS scripts and Alpha Prefab
    basic player movement & prefab folder was created

---
+++ OperationAlpha/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float sensitivity = 100f;
+    public Transform player;
+
+    float verticalRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input of Mouse Position
+        float x = Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
+        float y = Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+
+        //Vertical rotation
+        verticalRotation -= y;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 0f);
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+
+        //horizontal rotation
+        player.Rotate(Vector3.up * x);
+    }
+}
