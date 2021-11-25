fc61a3243074814daae90b682f818284b6f5c018
3cD0 <mrdavid3@yahoo.com>
2021-11-25 15:53:53 -0500

    Made Health Bar for player
    the health bar ui and script are implemented, demo is in intro_map.

---
+++ OperationAlpha/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Image healthBar;
+    public Text healthPercentage;
+
+    public float health,maxHealth = 100;
+    float lerpSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthPercentage.text = health + "%";
+
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+
+        lerpSpeed = 3f * Time.deltaTime;
+        Damage();
+        HealthBarFiller();
+        ColorChanger();
+        
+    }
+
+    void HealthBarFiller()
+    {
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, health / maxHealth, lerpSpeed);
+    }
+
+    void ColorChanger()
+    {
+        Color healthColor = Color.Lerp(Color.red, Color.green, (health / maxHealth));
+        healthBar.color = healthColor;
+    }
+
+    public void Damage()
+    {
+        if (Input.GetKeyDown(KeyCode.B) && health > 0)
+        {
+            health -= 5;
+        }
+    }
+}
