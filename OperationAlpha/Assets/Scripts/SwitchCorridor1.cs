ef2040c45427d200571476b13f3e2cfc053f4664
MinhT1 <MinhThienTran1@hotmail.com>
2021-12-02 16:12:33 -0500

    Fixed Corridor 2 and Made Boss Room 3
    Corridor 3 is integrated with Boss Room 3

--- OperationAlpha/Assets/Scripts/SwitchCorridor1.cs
+++ OperationAlpha/Assets/Scripts/SwitchCorridor1.cs
@@ -5,7 +5,7 @@ public class SwitchCorridor1 : MonoBehaviour
 {
     // This value is found by going in Unity -> File -> Build Settings 
     // -> Scenes In Build in top section -> number next to it
-    int Corridor1SceneNumber = 1;
+    int Corridor1SceneNumber = 2;
     public void OnTriggerEnter(Collider other)
     {
         SceneManager.LoadScene(Corridor1SceneNumber);
