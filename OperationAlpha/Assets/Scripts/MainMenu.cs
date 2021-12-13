a93e6fa0f8bb9f496c511c902cc86faad012d642
MinhT1 <56123321+MinhT1@users.noreply.github.com>
2021-12-13 12:28:59 -0500

    WinScreen, EndRoom, Bugfixes

--- OperationAlpha/Assets/Scripts/MainMenu.cs
+++ OperationAlpha/Assets/Scripts/MainMenu.cs
@@ -1,10 +1,27 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Intro_Map");
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene("New_MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
